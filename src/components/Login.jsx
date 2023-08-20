ac21761eb50288927dd313484765fa64793745fa
Peter Key <63628694+peterekey@users.noreply.github.com>
2023-08-21 08:05:51 +1000

    Initial commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import { useDispatch } from "react-redux"
+import { login, logout } from '../features/user'
+
+export default function Login() {
+    const dispatch = useDispatch()
+    return (
+        <>
+            <button onClick={() => {
+                dispatch(login({name: "Pedro", age: 20, email:"pedro@gmail.com"}))
+            }}>Login</button>
+            <button onClick={() => dispatch(logout())}>Logout</button>
+        </>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import userReducer from './features/user'
+import themeReducer from './features/theme'
+
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+    theme: themeReducer
+  }
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+)
